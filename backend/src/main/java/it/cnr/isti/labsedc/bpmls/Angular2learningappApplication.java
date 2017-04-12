fe0b71a643dca0d73636c89c9ca1b57e28e4f94e
Venkatapathy <venkat.eduseva@gmail.com>
2017-04-12 14:17:28 +0200

    changed file path of learning scenario related to jar output

--- backend/src/main/java/it/cnr/isti/labsedc/bpmls/Angular2learningappApplication.java
+++ backend/src/main/java/it/cnr/isti/labsedc/bpmls/Angular2learningappApplication.java
@@ -1,17 +1,41 @@
 package it.cnr.isti.labsedc.bpmls;
 
+import javax.annotation.PostConstruct;
+
 import org.camunda.bpm.spring.boot.starter.annotation.EnableProcessApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @EnableProcessApplication
-public class Angular2learningappApplication {
-
+public class Angular2learningappApplication{
+	
+	@Autowired
+    JdbcTemplate jdbcTemplate;
+	
 	public static void main(String[] args) {
+		
 		SpringApplication.run(Angular2learningappApplication.class, args);
 		
 		//
 		
+	}
+	
+	
+	
+	@PostConstruct
+    public void init(){
+		System.out.println("Creating learningscenarioinstance table");
+
+        jdbcTemplate.execute("DROP TABLE learningscenarioinstance IF EXISTS");
+        jdbcTemplate.execute("CREATE TABLE learningscenarioinstance(" +
+                "lsinstid SERIAL, learningscenarioid VARCHAR(255), processinstaneid VARCHAR(255))");
+
+    
 	}
 }
