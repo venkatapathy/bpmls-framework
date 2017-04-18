137cf71046917d1bc1e79544ed777ddd0aec3887
Venkatapathy <venkat.eduseva@gmail.com>
2017-04-18 13:09:02 +0200

    server side coding done

--- backend/src/main/java/it/cnr/isti/labsedc/bpmls/LearningProcessEngine.java
+++ backend/src/main/java/it/cnr/isti/labsedc/bpmls/LearningProcessEngine.java
@@ -6,6 +6,10 @@ import java.io.FileReader;
 import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Iterator;
@@ -29,8 +33,12 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -46,19 +54,23 @@ import it.cnr.isti.labsedc.bpmls.senarios.LearningScenario;
 @RestController
 public class LearningProcessEngine {
 	@Autowired
+    JdbcTemplate jdbcTemplate;
+	
+	
+	@Autowired
 	private RuntimeService runtimeService;
 
 	@Autowired
 	private TaskService taskService;
-	
+
 	@Autowired
 	private FormService formService;
-	
+
 	private String processInstanceId=null;
-	
+
 	private static String LEARNINGSCENARIO="learningscenario1";
-	
-	
+
+
 	private Resource learningScenarioXml;
 	/**
 	 * Given a learning scenario, it start the scenario runs each task and finishes it
@@ -66,78 +78,129 @@ public class LearningProcessEngine {
 	private void testLearningScenario(LearningScenario learningScenario){
 		//get the initial values
 		Map<String, Object> variables = new HashMap<String, Object>();
-		
+
 		Iterator<LearningScenario.InitialValuation.DataObject> i=learningScenario.getInitialValuation().getDataObject().iterator();
 		while(i.hasNext()){
 			LearningScenario.InitialValuation.DataObject dataObject=(LearningScenario.InitialValuation.DataObject)i.next();
 			variables.put(dataObject.getCamundaid(), dataObject.getValue());
 		}
-		
+
 		//start the learning scenario which is the bpmn process
 		System.out.println("starting the learning scenario and is associated bpmn process");
 		System.out.println("initial variables initated are");
 		for (String key : variables.keySet()) {
-		    System.out.println(key + " " + variables.get(key));
+			System.out.println(key + " " + variables.get(key));
 		}
-		
+
 		String processinstanceId=runtimeService.startProcessInstanceByKey(learningScenario.getProcessid(),variables).getId();
 		System.out.println("Starting successful");
-		
+
 		boolean isProcessRunning=true;
-		
+
 		while(isProcessRunning){
 			//get the current user task
 			Task task = taskService.createTaskQuery().processInstanceId(processinstanceId).singleResult();
 			if(task!=null){
 				System.out.println("Current task is: "+task.getName());
 				System.out.println("completing the current task and moving on");
-				
+
 				System.out.println("Generated form is");
-				
+
 				List<FormField> taskformsfields=formService.getTaskFormData(task.getId()).getFormFields();
-				
+
 				for(int temp=0; temp < taskformsfields.size();temp++){
 					System.out.println(taskformsfields.get(temp).getLabel());
 				}
-				
-				
+
+
 				//expected 
 				taskService.complete(task.getId());
 			}else{
 				isProcessRunning=false;
 				System.out.println("process run successfully");
 			}
-			
-			
+
+
 		}
-		
+
 	}
-	
-	
+
+
 	/**
-	 * This starts a learning scenario based on the id sent. 
-	 * @return json object with status true or false and id which is the processinstance id, if 
-	 * the starting is success
+	 * This starts a learning scenario based on the LearningScenario id sent as parameter.
+	 * @param lsid-ID of the LearningScenario 
+	 * @return json object with status true or false and id which is the learningscenarioinstance id, if 
+	 * the starting is success. Null if its a failure
 	 * @throws Exception
 	 */
-	@RequestMapping(value="/startlearningscenario")
-	public String startLearningScenario() throws Exception{
+	@RequestMapping(value="/startlearningscenario", method = RequestMethod.GET)
+	public String startLearningScenario(@RequestParam(value = "lsid") String lsid) throws Exception{
+		//temp setting
+		lsid="learningscenario.xml";
 		
-		learningScenarioXml=new UrlResource("classpath:schema/learningscenario.xml");
+		//get the learning scenario id sent as GET parameter
+		learningScenarioXml=new UrlResource("classpath:schema/"+lsid);
+		
+		//open the xml and convert it to JAVA object
 		//File f = new File("./src/main/resources/schema/learningscenario.xml");
 		JAXBContext jaxbContext = JAXBContext.newInstance(LearningScenario.class);
-		
-		
+
+
 		Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 		LearningScenario learningScenario = (LearningScenario) jaxbUnmarshaller.unmarshal(learningScenarioXml.getInputStream());
-		testLearningScenario(learningScenario);
-		return "success";
-		//when the learningscenario id comes check if it exists 
 		
+		//start a process instance given by the process id 
+		//get the initial values
+				Map<String, Object> variables = new HashMap<String, Object>();
+
+				Iterator<LearningScenario.InitialValuation.DataObject> i=learningScenario.getInitialValuation().getDataObject().iterator();
+				while(i.hasNext()){
+					LearningScenario.InitialValuation.DataObject dataObject=(LearningScenario.InitialValuation.DataObject)i.next();
+					variables.put(dataObject.getCamundaid(), dataObject.getValue());
+				}
+
+				//start the learning scenario which is the bpmn process
+				System.out.println("starting the learning scenario and is associated bpmn process");
+				System.out.println("initial variables initated are");
+				for (String key : variables.keySet()) {
+					System.out.println(key + " " + variables.get(key));
+				}
+
+				String processinstanceId=runtimeService.startProcessInstanceByKey(learningScenario.getProcessid(),variables).getId();
+				System.out.println("Starting successful");
+				
+				//done to be used within inner classLocal variable lsid defined in an enclosing scope must be final or effectively final
+				final String lsid_final=lsid;
+				
+				//insert the created pid and its corresponding learning scenario into the table and
+				//get its unique id
+				GeneratedKeyHolder holder = new GeneratedKeyHolder();
+				jdbcTemplate.update(new PreparedStatementCreator() {
+				    @Override
+				    public PreparedStatement createPreparedStatement(Connection con) throws SQLException {
+				        PreparedStatement statement = con.prepareStatement("INSERT INTO learningscenarioinstance(learningscenarioid, processinstaneid) VALUES (?, ?) ", Statement.RETURN_GENERATED_KEYS);
+				        statement.setString(1,lsid_final );
+				        statement.setString(2, processinstanceId);
+				        return statement;
+				    }
+				}, holder);
+
+				long primaryKey = holder.getKey().longValue();
+				String jsonreturn="{"
+						+ "status:success,"
+						+ "lsinstid:"+holder.getKey().toString()+","
+						+ "lsid:"+lsid_final+","
+						+ "pinstid:"+processinstanceId+","
+						+ "pid:"+learningScenario.getProcessid()
+						+ "}";
+		//testLearningScenario(learningScenario);
+		return jsonreturn;
+		//when the learningscenario id comes check if it exists 
+
 	}
 	/**
-	 * This gets the current task of a given learning scenarios. Currently hardcoded to get the current
-	 * task of learningscenario1 for a given process instance with a particular key
+	 * This gets the current task of a given learning scenarios instance id. 
+	 * 
 	 * TODO: Need to make the function more dynamic
 	 * @return
 	 * @throws Exception
@@ -146,40 +209,28 @@ public class LearningProcessEngine {
 	 * learning scenario
 	 */
 	@RequestMapping(value = "/getcurrenttask", method = RequestMethod.GET)
-	public String getCurrentLearningTask() throws Exception {
-		if(processInstanceId==null){
-			//TODO: 
-			//Here the process instance should start with inital values from
-			//the learningscenario.config
-			processInstanceId = runtimeService.startProcessInstanceByKey("ithelpdeskprocess").getProcessInstanceId();
-		}
+	public String getCurrentLearningTask(@RequestParam(value = "pinstid") String pinstid) throws Exception {
 		
+
 		//get the current task
 		Task task = taskService.createTaskQuery().processInstanceId(processInstanceId).singleResult();
-		System.out.println("./src/main/resources/learningscenarios/"+LEARNINGSCENARIO+"/"+task.getTaskDefinitionKey()+".html");
 		
-		File f = new File("./src/main/resources/learningscenarios/"+LEARNINGSCENARIO+"/"+task.getTaskDefinitionKey()+".html");
-		if(f.exists() && !f.isDirectory()) { 
-		    // send the whole html file
-			BufferedReader br = new BufferedReader(new FileReader(f));
-		    try {
-		        StringBuilder sb = new StringBuilder();
-		        String line = br.readLine();
-
-		        while (line != null) {
-		            sb.append(line);
-		            sb.append("\n");
-		            line = br.readLine();
-		        }
-		        return sb.toString();
-		    } finally {
-		        br.close();
-		    }
-		}
 		
-		return "no task: no task";
+		
+		return new HtmlFormEngine().renderFormData(formService.getTaskFormData(task.getId()));
 		//shortcut, check if the file with taskname.scenarioname exist! if so send it back as 
 		//check if the current task has a UI in the learningscenario config, else complete the task and move ahead
-		
+
+	}
+	
+	/**
+	 * Completes a task given the learning scenario instance id. It must also provide any form that needs to be completed
+	 * @param lsid
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping(value = "/completelearningtask", method = RequestMethod.GET)
+	public String completeLearningTask(@RequestParam(value = "lsid") String lsid) throws Exception{
+		return "all is well";
 	}
 }
